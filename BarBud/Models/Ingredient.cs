82b04d7d7c28ddb86ecb86b8303f4854f71fbcb8
Stefan Frisk <stefrisk@gmail.com>
2025-10-30 19:27:47 +0100

    Add project files.

---
+++ BarBud/Models/Ingredient.cs
@@ -0,0 +1,10 @@
+﻿namespace BarBud.Models
+{
+    public class Ingredient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Amount { get; set; } = string.Empty;
+        public string Unit { get; set; } = string.Empty;
+    }
+}
