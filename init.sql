de16bdf82a3fc1ae0e826d0f148a0dd64497d5d6
Stefrisk <stefrisk@gmail.com>
2025-11-21 19:21:07 +0100

    Initialize BarBud database and user

--- init.sql
+++ init.sql
@@ -1,1 +1,10 @@
-
+CREATE DATABASE [BarBud];  
+GO  
+  
+USE [BarBud];  
+GO  
+  
+CREATE LOGIN [barbud] WITH PASSWORD = 'bardev123';  
+CREATE USER [barbud] FOR LOGIN [barbud];  
+ALTER ROLE db_owner ADD MEMBER [barbud];  
+GO
