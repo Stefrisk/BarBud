ffdf566f988274dc77d7859a11fdc2d5021a5f7b
Anton Bohman <ante-2001@live.se>
2025-11-24 19:12:22 +0100

    #50 - Async Tasks - Tasks to connect to frontend.

--- BarBud/Components/Pages/Ingredients.razor.cs
+++ BarBud/Components/Pages/Ingredients.razor.cs
@@ -1,7 +1,46 @@
 using Microsoft.AspNetCore.Components;
-
+using BarBud.Db;
+using BarBud.Models;
+using MudBlazor;
+using BarBud.Services;
 namespace BarBud.Components.Pages;
 
 public partial class Ingredients : ComponentBase
 {
+    [Inject] IngredientFunctions IngredientService { get; set; } = default!;
+    
+    protected List<Ingredient> IngredientList = new();
+    protected string? ErrorMessage { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        await LoadIngredientsAsync();
+    }
+    protected async Task LoadIngredientsAsync() 
+    { 
+        IngredientList = await IngredientService.GetAllIngredientsAsync();
+    }
+    protected async Task AddIngredientAsync(Ingredient newIngredient) 
+    { 
+        ErrorMessage = null;
+        if (string.IsNullOrWhiteSpace(newIngredient.Name))
+        {
+            ErrorMessage = "An Ingredient name is required";
+            return;
+        }
+        
+        await IngredientService.AddAsync(newIngredient);
+        await LoadIngredientsAsync();
+    }
+    
+    protected async Task DeleteIngredientAsync(int id)
+    {
+        await IngredientService.DeleteAsync(id);
+        await LoadIngredientsAsync();
+    }
+    protected async Task UpdateIngredientAsync(Ingredient ingredient)
+    {
+        await IngredientService.UpdateAsync(ingredient);
+        await LoadIngredientsAsync();
+    }
 }
