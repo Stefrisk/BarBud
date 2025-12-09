66f04d98f23707490ffc58336e47023701fd9061
Anton Bohman <ante-2001@live.se>
2025-12-09 21:29:13 +0100

    not ready

--- BarBud/Components/Account/IdentityRedirectManager.cs
+++ BarBud/Components/Account/IdentityRedirectManager.cs
@@ -15,7 +15,7 @@ namespace BarBud.Components.Account
             MaxAge = TimeSpan.FromSeconds(5),
         };
 
-        [DoesNotReturn]
+        
         public void RedirectTo(string? uri)
         {
             uri ??= "";
@@ -28,11 +28,11 @@ namespace BarBud.Components.Account
 
             // During static rendering, NavigateTo throws a NavigationException which is handled by the framework as a redirect.
             // So as long as this is called from a statically rendered Identity component, the InvalidOperationException is never thrown.
-            navigationManager.NavigateTo(uri);
-            throw new InvalidOperationException($"{nameof(IdentityRedirectManager)} can only be used during static rendering.");
+            navigationManager.NavigateTo(uri, forceLoad: false);
+            
         }
 
-        [DoesNotReturn]
+       
         public void RedirectTo(string uri, Dictionary<string, object?> queryParameters)
         {
             var uriWithoutQuery = navigationManager.ToAbsoluteUri(uri).GetLeftPart(UriPartial.Path);
@@ -40,7 +40,7 @@ namespace BarBud.Components.Account
             RedirectTo(newUri);
         }
 
-        [DoesNotReturn]
+        
         public void RedirectToWithStatus(string uri, string message, HttpContext context)
         {
             context.Response.Cookies.Append(StatusCookieName, message, StatusCookieBuilder.Build(context));
@@ -49,10 +49,10 @@ namespace BarBud.Components.Account
 
         private string CurrentPath => navigationManager.ToAbsoluteUri(navigationManager.Uri).GetLeftPart(UriPartial.Path);
 
-        [DoesNotReturn]
+       
         public void RedirectToCurrentPage() => RedirectTo(CurrentPath);
 
-        [DoesNotReturn]
+        
         public void RedirectToCurrentPageWithStatus(string message, HttpContext context)
             => RedirectToWithStatus(CurrentPath, message, context);
     }
