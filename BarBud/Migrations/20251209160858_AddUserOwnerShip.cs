c85dae917e1078ac119b1e213a2e1c888f42656f
Anton Bohman <ante-2001@live.se>
2025-12-09 17:12:52 +0100

    Db update

---
+++ BarBud/Migrations/20251209160858_AddUserOwnerShip.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BarBud.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserOwnerShip : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Recipes",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Drinks",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Recipes_UserId",
+                table: "Recipes",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Drinks_UserId",
+                table: "Drinks",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Drinks_AspNetUsers_UserId",
+                table: "Drinks",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Recipes_AspNetUsers_UserId",
+                table: "Recipes",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Drinks_AspNetUsers_UserId",
+                table: "Drinks");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Recipes_AspNetUsers_UserId",
+                table: "Recipes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Recipes_UserId",
+                table: "Recipes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Drinks_UserId",
+                table: "Drinks");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Recipes");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Drinks");
+        }
+    }
+}
