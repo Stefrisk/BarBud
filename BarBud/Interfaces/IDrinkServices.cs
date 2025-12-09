3940dcf1c287ba45c94fd227ad7a6954b1d23a8f
Fredrik von Post <fredrik.v.post@gmail.com>
2025-12-09 10:37:33 +0100

    Update IDrinkServices interface and AddAsync return type
    Refactored IDrinkServices to have AddAsync return the added Drink instead of the full list. Added new methods for retrieving drinks by ID, name, and details. Updated DrinkFunctions implementation to match the new AddAsync signature.

--- BarBud/Interfaces/IDrinkServices.cs
+++ BarBud/Interfaces/IDrinkServices.cs
@@ -1,12 +1,14 @@
 ﻿using BarBud.Models;
 
-namespace BarBud.Interfaces
+namespace BarBud.Interfaces;
+
+public interface IDrinkServices
 {
-    public interface IDrinkServices
-    {
-        public Task<List<Drink>> GetAllDrinksAsync();
-        public Task<List<Drink>> AddAsync(Drink drink);
-        public Task<bool> DeleteAsync(int id);
-        public Task<bool> UpdateAsync(Drink drink);
-    }
+    Task<Drink> AddAsync(Drink drink);
+    Task<bool> DeleteAsync(int id);
+    Task<List<Drink>> GetAllDrinksAsync();
+    Task<Drink?> GetByIdAsync(int id);
+    Task<Drink?> GetByNameAsync(string name);
+    Task<Drink?> GetDetailsByIdAsync(int id);
+    Task<bool> UpdateAsync(Drink drink);
 }
